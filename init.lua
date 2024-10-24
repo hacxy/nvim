a8dcb33e2ee24c4dccf3bdf1f1aae5a6d7197643
hacxy <hacxy.js@outlook.com>
2024-10-24 16:16:05 +0800

    perf: 兼容vscode-neovim

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+  -- VSCode extension  (vscode打开时加载单独的配置)
+  require("config.vscode")
+else
+  -- ordinary Neovim   (常规打开加载LazyVim)
+  require("config.lazy")
+end
